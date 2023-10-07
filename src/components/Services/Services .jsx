a60aa15d5beec6beae5ac87f43058913909684cd
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-07 17:04:34 +0600

    add animation in service card

--- src/components/Services/Services .jsx
+++ src/components/Services/Services .jsx
@@ -2,6 +2,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
+import { BsFillArrowThroughHeartFill } from "react-icons/bs";
 
 const Services = () => {
   const { services } = useContext(AuthContext);
@@ -11,12 +12,18 @@ const Services = () => {
   }, []);
   return (
     <div className="container mx-auto">
-      <p data-aos="slide-up" className="text-3xl font-bold text-center my-20">
+      <p data-aos="slide-up" className="text-5xl font-bold text-center my-20">
         Our Services
       </p>
       <div className="grid px-4 gap-4 md:gap-6 xl:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {services.map(service => (
-          <div key={service.id} className="card  bg-base-100 shadow-xl">
+          <div
+            data-aos="slide-up"
+            data-aos-duration="600"
+            data-aos-anchor-placement="top-center	"
+            key={service.id}
+            className="card  bg-base-100 shadow-xl"
+          >
             <figure className="px-10 pt-10">
               <img
                 src={service.image}
@@ -25,11 +32,16 @@ const Services = () => {
               />
             </figure>
             <div className="card-body items-center text-center">
-              <h2 className="card-title text-custom-color">{service.name}</h2>
+              <h2 className="card-title text-3xl font-stylishFont text-custom-color">
+                {service.name}
+              </h2>
               <p>{service.price}</p>
               <p>{service.short_description}</p>
               <div className="card-actions">
-                <button className="btn btn-primary">View Details</button>
+                <button className="py-2 font-medium text-custom-color px-4 border-2 border-[#bc9b6a] rounded-full hover:bg-[#bc9b6a] hover:text-white duration-700 flex items-center gap-2">
+                  View Details{" "}
+                  <BsFillArrowThroughHeartFill></BsFillArrowThroughHeartFill>
+                </button>
               </div>
             </div>
           </div>
