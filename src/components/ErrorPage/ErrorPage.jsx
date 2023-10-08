0b95763b00add24d683ed35b2c52b1a9e1d7611e
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-08 20:26:48 +0600

    modify error page style

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,30 @@
 import { Link } from "react-router-dom";
+import errorImg from "../../assets/err.png";
+import { BsChevronDoubleLeft } from "react-icons/bs";
 
 const ErrorPage = () => {
   return (
-    <div className="h-screen bg-[#a3a3a39d] flex justify-center items-center">
-      <div className="bg-white p-10 rounded-xl flex gap-4 flex-col items-center justify-center">
-        <h2 className="text-5xl">Page not found</h2>
-        <Link to="/">
-          <button className="btn btn-ghost underline text-blue-700 font-bold">
-            Go Home
-          </button>
-        </Link>
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex  flex-col lg:flex-row justify-between container items-center mx-auto">
+        <div>
+          <img src={errorImg} alt="error img" />
+        </div>
+        <div className="border p-10 rounded-lg">
+          <h2 className="text-5xl">Page not found</h2>
+          <Link to="/">
+            <button className="btn mt-4 btn-ghost text-blue-700 font-bold">
+              <BsChevronDoubleLeft></BsChevronDoubleLeft>
+              Go Back Our Side
+            </button>
+          </Link>
+          <hr />
+          <p className="max-w-sm">
+            Our apologies, but it seems that something has gone wrong. We are
+            experiencing technical difficulties and are actively working to
+            resolve the issue. Please try again later, or contact our support
+            team for assistance. Thank you for your patience.
+          </p>
+        </div>
       </div>
     </div>
   );
