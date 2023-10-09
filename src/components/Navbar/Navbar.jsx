a045edade25c9cda2b5e0033d41f48ee6b0dce3b
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-09 17:53:28 +0600

    fixed some bug

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -141,7 +141,7 @@ const Navbar = () => {
               <div className="dropdown dropdown-end">
                 <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                   <div className="w-10 rounded-full">
-                    <img src={user.photoURL} />
+                    <img src={user?.photoURL} />
                   </div>
                 </label>
                 <ul
